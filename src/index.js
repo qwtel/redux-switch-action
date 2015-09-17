7b3dbf12d0d51b78b5c09d941a0da9281655ae4f
Florian Klampfer <f.klampfer@gmail.com>
2015-09-17 21:53:50 +0200

    create switch action

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-export function add(x, y) {
-  return x + y;
-}
+export default (actionToReducerMap) => {
+  return function switchAction(state, action = {}) {
+    const reducer = actionToReducerMap[action.type];
+    return reducer ? this::reducer(state, action.payload || {}) : state;
+  };
+};
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,69 @@
 import expect from 'expect';
-import { add } from '../src';
+import createSwitchAction from '../src';
 
-describe('add', () => {
-  it('should add 2 and 2', () => {
-    expect(add(2, 2)).toBe(4);
+describe('createSwitchAction', () => {
+  it('should exist', () => {
+    expect(typeof createSwitchAction !== 'undefined').toBe(true);
+  });
+
+  const ADD = 'ADD';
+  const SUB = 'SUB';
+  const OTHER = 'OTHER';
+  const INC = 'INC';
+
+  function addReducer(state, {amount}) {
+    return state + amount;
+  }
+
+  function subReducer(state, {amount}) {
+    return state - amount;
+  }
+
+  function incReducer(state) {
+    return state + 1;
+  }
+
+  const switchAction = createSwitchAction({
+    [ADD]: addReducer,
+    [SUB]: subReducer,
+    [INC]: incReducer,
+  });
+
+  it('should call a reducer according to an action type', () => {
+    const addAction = {
+      type: ADD,
+      payload: {
+        amount: 2,
+      },
+    };
+
+    const subAction = {
+      type: SUB,
+      payload: {
+        amount: 2,
+      },
+    };
+
+    expect(switchAction(5, addAction)).toBe(7);
+    expect(switchAction(5, subAction)).toBe(3);
+  });
+
+  it('should return the state for an unknown action', () => {
+    const otherAction = {
+      type: OTHER,
+      payload: {
+        other: 'data',
+      },
+    };
+
+    expect(switchAction(5, otherAction)).toBe(5);
+  });
+
+  it('should accept actions without payload', () => {
+    const incAction = {
+      type: INC,
+    };
+
+    expect(switchAction(5, incAction)).toBe(6);
   });
 });
